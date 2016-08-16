3af5b3a0bcb081f445bf5547efb1f7502ee82c91
James <james_baster@shelter.org.uk>
2016-08-16 10:56:00 +0100

    On stats filter, have preset month filters you can just click

---
+++ src/FeedbackFieldBundle/Twig/StatsFiltersDatesExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace FeedbackFieldBundle\Twig;
+
+class StatsFiltersDatesExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('statsFiltersDates', array($this, 'statsFiltersDates')),
+        );
+    }
+
+    public function statsFiltersDates()
+    {
+
+        $out = array();
+
+        $create = new \DateTime();
+        $subInterval = new \DateInterval("P1M");
+
+        for ($i = 1; $i <= 4; $i++) {
+
+
+            $from = clone $create;
+            $from->setTime(0,0,0);
+            $from->setDate($from->format("Y"), $from->format("m"), 1);
+
+            $to = clone $create;
+            $to->setTime(23,59,59);
+            $to->setDate($from->format("Y"), $from->format("m"), cal_days_in_month(CAL_GREGORIAN, $from->format("m"), $from->format("Y")));
+
+            $out[] = array(
+                'label' => $create->format("M Y"),
+                'from' => $from,
+                'to' => $to,
+            );
+
+            $create->sub($subInterval);
+        }
+
+
+        return $out;
+    }
+
+    public function getName()
+    {
+        return 'StatsFiltersDatesExtension';
+    }
+}
+
